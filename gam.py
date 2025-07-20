483f1b96b7b10cd60ec8ab1006b67942a5ef5f66
vathsalatherase <vathsalatherase@karunya.edu.in>
2025-07-20 15:09:33 +0530

    Create gam.py

---
+++ gam.py
@@ -0,0 +1,25 @@
+import random
+
+# Generate a random number between 1 and 100
+secret_number = random.randint(1, 100)
+attempts = 0
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+while True:
+    try:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too low! Try again.")
+        elif guess > secret_number:
+            print("Too high! Try again.")
+        else:
+            print(f"Correct! The number was {secret_number}.")
+            print(f"You guessed it in {attempts} attempts.")
+            break
+
+    except ValueError:
+        print("Invalid input! Please enter a valid integer.")
